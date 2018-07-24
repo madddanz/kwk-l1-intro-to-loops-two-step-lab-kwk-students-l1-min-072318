283c950ce2e07733613b04fdc6f5ab91272e308d
madddanz <maddiedanzberger@gmail.com>
2018-07-24 20:54:08 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def first_steps
   Write a loop that outputs "Right foot back", "Left foot back",
   "Right foot back", and "Stop", with sleep after each
@@ -35,6 +36,47 @@ def a_few_more_steps
     sleep(1)
   end
 end
+=======
+# def first_steps
+#   Write a loop that outputs "Right foot back", "Left foot back",
+#   "Right foot back", and "Stop", with sleep after each
+#   loop do  
+#     puts "right foot back"
+#     sleep(0.5)
+#     puts "Left foot back"
+#     sleep(0.5)
+#     puts "Right foot back"
+#     sleep(0.5)
+#     puts "Stop"
+#     sleep(1)
+#   end
+# end
+
+# first_steps
+
+# def a_few_more_steps
+#   loop do  
+#     puts "right foot back"
+#     sleep(0.5)
+#     puts "Left foot back"
+#     sleep(0.5)
+#     puts "Right foot back"
+#     sleep(0.5)
+#     puts "Stop"
+#     sleep(0.5)
+#     puts "right foot steps right and back"
+#     sleep(0.5)
+#     puts "Left foot crosses over right"
+#     sleep(0.5)
+#     puts "Right foot steps right"
+#     sleep(0.5)
+#     puts "Turn"
+#     sleep(1)
+#   end
+# end
+
+# puts a_few_more_steps
+>>>>>>> 060e7cd01e0121e9c1047da68339a55e99f520cd
 
 puts a_few_more_steps
 
@@ -52,6 +94,7 @@ def how_many_steps?
   end
 end
 
+<<<<<<< HEAD
 
 
 def break_dance
@@ -60,14 +103,27 @@ def break_dance
     steps += 1
   puts steps
     if (steps%2 == 0)
+=======
+how_many_steps?
+
+def break_dance
+   steps = 0 
+   until steps == 6
+    steps += 1
+  puts steps
+    if steps%2 == 0
+>>>>>>> 060e7cd01e0121e9c1047da68339a55e99f520cd
       puts "Left"
     else
       puts "Right"
     end
+<<<<<<< HEAD
     if steps == 6
     break
     end
   sleep(0.5)
+=======
+>>>>>>> 060e7cd01e0121e9c1047da68339a55e99f520cd
   end
 end
 
